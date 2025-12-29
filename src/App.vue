8110354fe5544852745865c92ff4ac95850965ee
sbsteacher <id@gmail.com>
2025-12-29 11:38:23 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!-- 로직 담당 -->
+<script setup>
+import FrontWheel from '@/components/FrontWheel.vue';
+import RearWheel from './components/RearWheel.vue';
+</script>
+
+<!-- 화면 (View) 담당 -->
+<template>
+  <FrontWheel></FrontWheel>
+  <RearWheel />
+</template>
+
+<!--
+컴포넌트(Component): 재사용 가능한 요소
+
+쉽게 생각해 자동차 부품이라고 봐도 된다. 
+
+-->
