9dbbd98c4d3e86a96ea336d4c0f677700ccf4f76
sbsteacher <id@gmail.com>
2025-12-29 14:36:26 +0900

    page98(2)

--- src/views/Page98.vue
+++ src/views/Page98.vue
@@ -1,9 +1,21 @@
 <script setup>
-
+let loading = false;
+const parent = ['fruits', 'abc'];
+const child = 'item';
 </script>
 
 <template>
-    <h1>Page98.vue</h1>
+    <h3>Page98.vue</h3>
+    <input type="text" placeholder="아이디" v-bind:readonly="loading"> 
+    <input type="password" placeholder="비밀번호" :readonly="loading"> 
+    <button :disabled="loading">로그인</button>
+    <hr>
+    <ul :class="parent" class="fruit">
+        <li :class="child">사과</li>
+        <li :class="child">바나나</li>
+        <li :class="child">딸기</li>
+        <li :class="child">배</li>
+    </ul>
 </template>
 
 <style scoped>
