9a196f2c4ef373611ca6e4d18206a0fd37b2797d
sbsteacher <id@gmail.com>
2026-01-13 13:01:40 +0900

    airbnb(4)

--- src/views/airbnb.vue
+++ src/views/airbnb.vue
@@ -28,10 +28,16 @@ onMounted(async () => {
           <path fill="none"d="m12 4 11.3 11.3a1 1 0 0 1 0 1.4L12 28"></path></svg>
         </span>
         <div class="d-flex">
-          <div v-for="subItem in item.sectionData.items" :key="subItem.demandStayListing.id" class="item">
+          <div v-for="subItem in item.sectionData.items" :key="subItem.demandStayListing.id" class="item po-rel">
             <img :src="subItem.contextualPictures[0].picture" alt="">
-            <div>타이틀</div>
-          </div>
+            <div>{{ subItem.title }}</div>
+            <div>{{ subItem.structuredContent.primaryLine[0].body }}</div>
+            <div>{{ subItem.structuredDisplayPrice.primaryLine.price }} 
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 8px; width: 8px; fill: currentcolor;"><path fill-rule="evenodd" d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"></path></svg>
+                  {{ subItem.avgRatingLocalized }}
+            </div>
+            <div v-if="subItem.badges.length" class="badge">{{ subItem.badges[0].text }}</div>
+          </div>          
         </div>
     </div>
   </div>
@@ -39,9 +45,16 @@ onMounted(async () => {
 
 <style scoped>
 .d-flex { display: flex; overflow-x: scroll; }
+.po-rel { position: relative; }
+
+.po-rel .badge { position: absolute; top: 10px; left: 10px; 
+  background-color: #ffffffab; padding: 5px 8px; 
+  border-radius: 10%;
+}
+
 .d-flex .item { width: 200px; }
 .d-flex .item img { width: 200px; }
-.right svg {
+svg {
   display: inline !important;
 }
 div.sections { width: 80vw; }
