e0917e801cace55af9b09fda9d05b15f37f4fc69
sbsteacher <id@gmail.com>
2026-01-13 14:51:34 +0900

    airbnb-component(2)

--- src/views/airbnb.vue
+++ src/views/airbnb.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, reactive } from 'vue';
+import LodgingItem from '@/components/LodgingItem.vue';
 import axios from 'axios';
 
 const state = reactive({
@@ -27,7 +28,17 @@ onMounted(async () => {
           style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 5.33333; overflow: visible;">
           <path fill="none"d="m12 4 11.3 11.3a1 1 0 0 1 0 1.4L12 28"></path></svg>
         </span>
-        <div class="d-flex">          
+        <div class="d-flex">   
+          <lodging-item v-for="subItem in item.sectionData.items" 
+              :key="subItem.demandStayListing.id"
+              :picture="subItem.contextualPictures[0].picture" 
+              :alt="subItem.title"
+              :title="subItem.title"
+              :period="subItem.structuredContent.primaryLine[0].body"
+              :price="subItem.structuredDisplayPrice.primaryLine.price"
+              :rating="subItem.avgRatingLocalized"
+              :badge="subItem.badges.length ? subItem.badges[0].text: ''"
+          />
           <!-- 
           <div v-for="subItem in item.sectionData.items" :key="subItem.demandStayListing.id" class="item po-rel">
             <img :src="subItem.contextualPictures[0].picture" alt="">
